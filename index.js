ac44b2f6879834d6934a2c4675746571b0357624
kevin <kevin.chumbay.07@est.ucacue.edu.ec>
2024-06-12 11:30:52 -0500

    nuevo index

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
-const representanteRoutes = require('./routes/representanteRoutes')
-const estudianteRoutes=require('./routes/estudianteRoutes')
-const cursoRoutes = require('./routes/cursoRoutes')
-const atrasoRoutes = require('./routes/atrasoRoutes')
-const asignaturaRoutes = require('./routes/asignaturaRoutes')
-const docenteRoutes = require('./routes/docenteRoutes')
-const esquelaRoutes = require('./routes/esquelaRoutes')
-const asignacionDocenteMateria=require('./routes/asignacionDocenteMateriaRoutes')
-const app = express();
+const representanteRoutes = require('./routes/representanteRoutes');
+const estudianteRoutes = require('./routes/estudianteRoutes');
+const cursoRoutes = require('./routes/cursoRoutes');
+const atrasoRoutes = require('./routes/atrasoRoutes');
+const asignaturaRoutes = require('./routes/asignaturaRoutes');
+const docenteRoutes = require('./routes/docenteRoutes');
+const esquelaRoutes = require('./routes/esquelaRoutes');
+const asignacionDocenteMateriaRoutes = require('./routes/asignacionDocenteMateriaRoutes');
 const cors = require('cors');
-app.use(cors());
+
+const app = express();
+
+// Configuración de CORS
+const corsOptions = {
+  origin: ['https://aplicativo-antonio-avila.vercel.app', 'http://localhost:3000'], // Agrega aquí los orígenes permitidos
+  optionsSuccessStatus: 200,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization"
+};
+app.use(cors(corsOptions));
+
 process.env.TZ = 'America/Guayaquil';
 
-app.use(express.json({limit: '500mb'}));
+app.use(express.json({ limit: '500mb' }));
 app.use('/representante', representanteRoutes);
 app.use('/curso', cursoRoutes);
 app.use('/user', userRoutes);
 app.use('/estudiante', estudianteRoutes);
-app.use('/atraso', atrasoRoutes )
-app.use('/asignatura' , asignaturaRoutes)
-app.use('/docente' , docenteRoutes)
-app.use('/esquela', esquelaRoutes )
-app.use('/docenteMateria', asignacionDocenteMateria )
-
-
+app.use('/atraso', atrasoRoutes);
+app.use('/asignatura', asignaturaRoutes);
+app.use('/docente', docenteRoutes);
+app.use('/esquela', esquelaRoutes);
+app.use('/docenteMateria', asignacionDocenteMateriaRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  
   res.status(500).send({ error: 'Algo salió mal' });
 });
 
-
 // Configuración del servidor para escuchar en todas las interfaces de red
-const PORT = 3000;  // Puedes cambiar el puerto si es necesario
+const PORT = process.env.PORT || 3000;  // Vercel configurará automáticamente el puerto
 const HOST = '0.0.0.0'; // Escucha en todas las interfaces de red
 app.listen(PORT, HOST, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`);
+  console.log(`Server running`);
   console.log('Zona horaria del servidor:', process.env.TZ);
 });
